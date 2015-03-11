931fad895227f416eab4a9c0a312a19aeced2260
Adam Ahrens <adam.ahrens@smartthings.com>
2015-03-11 08:41:02 -0500

    Adding Realm and a Noise model

---
+++ Noisily/Noisily/Models/Noise.swift
@@ -0,0 +1,14 @@
+//
+//  Noise.swift
+//  Noisily
+//
+//  Created by Adam Ahrens on 3/11/15.
+//  Copyright (c) 2015 Appsbyahrens. All rights reserved.
+//
+
+import UIKit
+import Realm
+
+class Noise: RLMObject {
+   
+}
